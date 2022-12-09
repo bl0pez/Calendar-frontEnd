ab0d9b52152a673545362920db53a3c986299fa4
b0pez <blopez4434@gmail.com>
2022-12-09 19:03:25 -0300

    add authSlice

---
+++ src/store/auth/authSlice.js
@@ -0,0 +1,14 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+export const authSlice = createSlice({
+    name: 'auth',
+    initialState: {
+        isDateModalOpen: false
+    },
+    reducers: {
+        
+    }
+});
+
+export const {  } = authSlice.actions;
