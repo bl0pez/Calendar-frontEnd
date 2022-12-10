4429e431eb41948490b10decdff9d752b09cc38a
b0pez <blopez4434@gmail.com>
2022-12-10 02:32:54 -0300

    camptura de login y register

--- src/auth/pages/Login.jsx
+++ src/auth/pages/Login.jsx
@@ -1,17 +1,47 @@
+import { useForm } from '../../hooks';
 import './login.css';
 
+const loginFormFields = {
+    loginEmail: "",
+    loginPassword: "",
+}
+
+const registerFormFields = {
+    registerName: "",
+    registerEmail: "",
+    registerPassword: "",
+    registerPassword2: "",
+}
+
 export const Login = () => {
+
+    const { loginEmail, loginPassword, onInputChange:onLoginInputChange } = useForm( loginFormFields );
+    const { registerName, registerEmail, registerPassword, registerPassword2, onInputChange:onRegisterInputChange } = useForm( registerFormFields );
+
+    const loginSubmit = (e) => {
+        e.preventDefault();
+
+    }
+
+    const registerSubmit = (e) => {
+        e.preventDefault();
+
+    }
+
     return (
         <div className="container login-container">
             <div className="row">
                 <div className="col-md-6 login-form-1">
                     <h3>Ingreso</h3>
-                    <form>
+                    <form onSubmit={loginSubmit}>
                         <div className="form-group mb-2">
                             <input
                                 type="text"
                                 className="form-control"
                                 placeholder="Correo"
+                                name='loginEmail'
+                                value={ loginEmail }
+                                onChange={ onLoginInputChange }
                             />
                         </div>
                         <div className="form-group mb-2">
@@ -19,6 +49,9 @@ export const Login = () => {
                                 type="password"
                                 className="form-control"
                                 placeholder="Contraseña"
+                                name='loginPassword'
+                                value={ loginPassword }
+                                onChange={ onLoginInputChange }
                             />
                         </div>
                         <div className="form-group mb-2">
@@ -33,12 +66,15 @@ export const Login = () => {
 
                 <div className="col-md-6 login-form-2">
                     <h3>Registro</h3>
-                    <form>
+                    <form onSubmit={registerSubmit}>
                         <div className="form-group mb-2">
                             <input
                                 type="text"
                                 className="form-control"
                                 placeholder="Nombre"
+                                name='registerName'
+                                value={ registerName }
+                                onChange={ onRegisterInputChange }
                             />
                         </div>
                         <div className="form-group mb-2">
@@ -46,6 +82,9 @@ export const Login = () => {
                                 type="email"
                                 className="form-control"
                                 placeholder="Correo"
+                                name='registerEmail'
+                                value={ registerEmail }
+                                onChange={ onRegisterInputChange }
                             />
                         </div>
                         <div className="form-group mb-2">
@@ -53,6 +92,9 @@ export const Login = () => {
                                 type="password"
                                 className="form-control"
                                 placeholder="Contraseña"
+                                name='registerPassword'
+                                value={ registerPassword }
+                                onChange={ onRegisterInputChange }
                             />
                         </div>
 
@@ -61,6 +103,9 @@ export const Login = () => {
                                 type="password"
                                 className="form-control"
                                 placeholder="Repita la contraseña"
+                                name='registerPassword2'
+                                value={ registerPassword2 }
+                                onChange={ onRegisterInputChange }
                             />
                         </div>
 
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,11 +1,15 @@
 import { Navigate, Route, Routes } from 'react-router-dom'; 
+import { Login } from '../auth/pages/Login';
 import { Calendar } from '../calendar';
 
 export const AppRouter = () => {
 
   return (
     <Routes>
-          <Route path="/" element={<Calendar />} />   
+          {/* <Route path="/" element={<Calendar />} />    */}
+
+          <Route path="/auth/login" element={<Login />} />
+
     </Routes>
   )
 }
